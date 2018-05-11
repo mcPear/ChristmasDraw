d7235ba69f402144e82cfb4773788bd0c8e838f8
Crackaw <crackaw@outlook.com>
2018-05-11 06:02:08 +0200

    Allow to edit user from admin panel

--- src/client/src/app/user-maneger/user-maneger.component.ts
+++ src/client/src/app/user-maneger/user-maneger.component.ts
@@ -2,6 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 import {UserDto} from "../shared/dto/user.dto";
 import {MatDialog} from "@angular/material";
 import {UserService} from "../shared/service/user.service";
+import {UserEditComponent} from "../user-edit/user-edit.component";
+import {AppCacheStorage} from "../shared/storage/app-cache-storage";
 
 @Component({
   selector: 'app-user-maneger',
@@ -22,6 +24,24 @@ export class UserManegerComponent implements OnInit {
   async deleteUser(user){
     await this.service.deleteUser(user.preferredUsername);
     this.users = this.users.filter(item => item !== user);
+  }
+
+  showDetails(item) : void {
+    console.log(item);
+    let dialogRef = this.dialog.open(UserEditComponent, {
+      height: '600px',
+      width: '700px',
+      data: item
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      console.log(result);
+      if (result) {
+        this.cacheStorage.updateUserDto(result)
+          .then(res => this.ngOnInit())
+          .catch(err => console.log(err));
+      }
   }
 
 }
